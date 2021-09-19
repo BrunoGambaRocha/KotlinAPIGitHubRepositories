786c64735de1f08df8e2997cc94b0f1b49ad7a88
BrunoGambaRocha <bruno.roch@gmail.com>
2021-09-19 17:34:52 -0300

    create search menu app

--- app/src/main/java/br/com/brunoti/githubrepositories/ui/MainActivity.kt
+++ app/src/main/java/br/com/brunoti/githubrepositories/ui/MainActivity.kt
@@ -3,13 +3,12 @@ package br.com.brunoti.githubrepositories.ui
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
 import br.com.brunoti.githubrepositories.R
 import br.com.brunoti.githubrepositories.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener {
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
@@ -18,6 +17,27 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        val searchView = menu.findItem(R.id.action_search).actionView as SearchView
+        searchView.setOnQueryTextListener(this)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        Log.e(TAG, "onQueryTextSubmit: $query")   //atalho: loge
+        return true
+    }
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        Log.e(TAG, "onQueryTextChange: $newText")   //atalho: loge
+        return false
+    }
+
+    companion object {
+        private const val TAG = "TAG"
     }
 
 }
